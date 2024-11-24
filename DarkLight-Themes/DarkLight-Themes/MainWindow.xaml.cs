659643acac4f5984b24db814e4f356cf6451b3cb
Hussein Md <h.mdeihly@hotmail.com>
2024-11-24 15:33:09 +0100

    added themes dark-mode

---
+++ DarkLight-Themes/DarkLight-Themes/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DarkLight_Themes
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            loadGurrentTheme();
+        }
+
+        private void loadGurrentTheme()
+        {
+            // Check saved theme and set the corresponding RadioButton
+            string savedTheme = Properties.Settings.Default.ColorMode;
+
+            switch (savedTheme)
+            {
+                case "Dark":
+                    darkModeBtn.IsChecked = true;
+                    break;
+                case "Light":
+                    lightModeBtn.IsChecked = true;
+                    break;
+                default:
+                    // Set a default if no saved value
+                    lightModeBtn.IsChecked = true;
+                    break;
+            }
+        }
+        #region events
+        private void darkModeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Properties.Settings.Default.ColorMode = "Dark";
+            Properties.Settings.Default.Save();
+        }
+
+        private void lightModeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Properties.Settings.Default.ColorMode = "Light";
+            Properties.Settings.Default.Save();
+        }
+        #endregion
+    }
+}
